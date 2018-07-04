0de82788c43eee16250c22908fd248fec9abaddd
Gerónimo Afonso <geronimo.afonso@mikroways.net>
2018-07-04 13:03:41 -0300

    test gelf.go

--- gelf.go
+++ gelf.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"net"
 	"os"
-	"strings"
 	"time"
+	"strings"
 
-	"github.com/Graylog2/go-gelf/gelf"
 	"github.com/gliderlabs/logspout/router"
 )
 
@@ -21,57 +21,59 @@ func init() {
 
 // GelfAdapter is an adapter that streams UDP JSON to Graylog
 type GelfAdapter struct {
-	writer *gelf.Writer
-	route  *router.Route
+	conn  net.Conn
+	route *router.Route
 }
 
 // NewGelfAdapter creates a GelfAdapter with UDP as the default transport.
 func NewGelfAdapter(route *router.Route) (router.LogAdapter, error) {
-	_, found := router.AdapterTransports.Lookup(route.AdapterTransport("udp"))
+	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport("udp"))
 	if !found {
 		return nil, errors.New("unable to find adapter: " + route.Adapter)
 	}
 
-	gelfWriter, err := gelf.NewWriter(route.Address)
+	conn, err := transport.Dial(route.Address, route.Options)
 	if err != nil {
 		return nil, err
 	}
 
 	return &GelfAdapter{
-		route:  route,
-		writer: gelfWriter,
+		route: route,
+		conn:  conn,
 	}, nil
 }
 
 // Stream implements the router.LogAdapter interface.
 func (a *GelfAdapter) Stream(logstream chan *router.Message) {
-	for message := range logstream {
-		m := &GelfMessage{message}
-		level := gelf.LOG_INFO
-		if m.Source == "stderr" {
-			level = gelf.LOG_ERR
+	for m := range logstream {
+
+		msg := GelfMessage{
+			Version:        "1.1",
+      			Host:           hostname, // Running as a container cannot discover the Docker Hostname
+			ShortMessage:   m.Data,
+			Timestamp:      m.Time.Format(time.RFC3339Nano),
+			ContainerId:    m.Container.ID,
+			ContainerName:  m.Container.Name,
+			ContainerCmd:   strings.Join(m.Container.Config.Cmd," "),
+			ImageId:        m.Container.Image,
+			ImageName:      m.Container.Config.Image,
 		}
-		extra, err := m.getExtraFields()
+
+		if m.Source == "stdout" {
+      			msg.Level = 3
+    		}
+    		
+    		if m.Source == "stderr" {
+    			msg.Level = 6
+		}
+
+		js, err := json.Marshal(msg)
 		if err != nil {
 			log.Println("Graylog:", err)
 			continue
 		}
-
-		msg := gelf.Message{
-			Version:  "1.1",
-			Host:     hostname,
-			Short:    m.Message.Data,
-			TimeUnix: float64(m.Message.Time.UnixNano()/int64(time.Millisecond)) / 1000.0,
-			Level:    level,
-			RawExtra: extra,
-		}
-		// 	ContainerId:    m.Container.ID,
-		// 	ContainerImage: m.Container.Config.Image,
-		// 	ContainerName:  m.Container.Name,
-		// }
-
-		// here be message write.
-		if err := a.writer.WriteMessage(&msg); err != nil {
+		_, err = a.conn.Write(js)
+		if err != nil {
 			log.Println("Graylog:", err)
 			continue
 		}
@@ -79,34 +81,17 @@ func (a *GelfAdapter) Stream(logstream chan *router.Message) {
 }
 
 type GelfMessage struct {
-	*router.Message
+	Version      string  `json:"version"`
+	Host         string  `json:"host,omitempty"`
+	ShortMessage string  `json:"short_message"`
+	FullMessage  string  `json:"message,omitempty"`
+	Timestamp    string  `json:"timestamp,omitempty"`
+	Level        int     `json:"level,omitempty"`
+
+	ImageId        string `json:"image_id,omitempty"`
+	ImageName      string `json:"image_name,omitempty"`
+	ContainerId    string `json:"container_id,omitempty"`
+	ContainerName  string `json:"container_name,omitempty"`
+	ContainerCmd   string `json:"command,omitempty"`
 }
 
-func (m GelfMessage) getExtraFields() (json.RawMessage, error) {
-
-	_status, _ := json.Marshal(m.Container.State)
-	extra := map[string]interface{}{
-		"_container_id":   m.Container.ID,
-		"_container_name": m.Container.Name[1:], // might be better to use strings.TrimLeft() to remove the first /
-		"_image_id":       m.Container.Image,
-		"_image_name":     m.Container.Config.Image,
-		"_command":        strings.Join(m.Container.Config.Cmd[:], " "),
-		"_created":        m.Container.Created,
-		"_status":         _status,
-	}
-	for name, label := range m.Container.Config.Labels {
-		if len(name) > 5 && strings.ToLower(name[0:5]) == "gelf_" {
-			extra[name[4:]] = label
-		}
-	}
-	swarmnode := m.Container.Node
-	if swarmnode != nil {
-		extra["_swarm_node"] = swarmnode.Name
-	}
-
-	rawExtra, err := json.Marshal(extra)
-	if err != nil {
-		return nil, err
-	}
-	return rawExtra, nil
-}
